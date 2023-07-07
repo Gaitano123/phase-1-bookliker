06269cf566e95c6079407f6505d52e4406437f27
Gaitano <gaitano.george@student.moringaschool.com>
2023-07-07 16:52:52 +0300

    Completed Assignment

--- js/index.js
+++ js/index.js
@@ -1,32 +1,82 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('http://localhost:3000/books')
-    .then(res => res.json())
-    .then(books => {
-        books.forEach(book => settingBooks(book));
-    })
+document.addEventListener("DOMContentLoaded", () => {
 
-    function settingBooks(book) {
-        const list = document.createElement('li')
-        list.innerHTML = `<p>${book.title}</p>`
-        list.addEventListener('click', () => showInfo(book))
-        document.querySelector('#list').appendChild(list)
-    }
-
-    const users = `${books.users}`;
-    const username = users.map(user => `${user.username}`)
-    console.log(username)
-
-    function showInfo(book) {
-        const pack = document.createElement('div')
-        pack.innerHTML= `
-        <p>${book.id}</P>
-        <img src="${book.img_url}"/>
-        <p>${book.author}</P>
-        <p>${book.subtitle}</P>
-        <p>${book.description}</P>
-        <p>${book.users}</P>
-        <button>like</button>
-        `
-        document.querySelector('#show-panel').appendChild(pack)
-    }
-});
+    // Fetch the list of books from the backend
+ fetch('http://localhost:3000/books')
+ .then(response => response.json())
+ .then(books => {
+   const listElement = document.querySelector('#list');
+ 
+   // Display the book titles in the list
+   books.forEach(book => {
+     const li = document.createElement('li');
+     li.textContent = book.title;
+     li.addEventListener('click', () => {
+       // Show book details when the title is clicked
+       showBookDetails(book);
+     });
+     listElement.appendChild(li);
+   });
+ });
+ 
+ // Function to display book details
+ function showBookDetails(book) {
+ const showPanel = document.querySelector('#show-panel');
+ showPanel.innerHTML = ''; // Clear previous content
+ 
+ // Create elements for book details
+ const thumbnail = document.createElement('img');
+ thumbnail.src = book.thumbnail;
+ const description = document.createElement('p');
+ description.textContent = book.description;
+ const likedBy = document.createElement('ul');
+ 
+ // Display list of users who liked the book
+ book.users.forEach(user => {
+   const userLi = document.createElement('li');
+   userLi.textContent = user.username;
+   likedBy.appendChild(userLi);
+ });
+ 
+ // Create like button
+ const likeButton = document.createElement('button');
+ likeButton.textContent = 'Like';
+ likeButton.addEventListener('click', () => {
+   const currentUser = { id: 1, username: 'pouros' };
+   const likedByUsers = [...book.users, currentUser];
+   likeBook(book.id, likedByUsers);
+ });
+ 
+ // Append elements to the show panel
+ showPanel.appendChild(thumbnail);
+ showPanel.appendChild(description);
+ showPanel.appendChild(likedBy);
+ showPanel.appendChild(likeButton);
+ }
+ 
+ // Function to like a book
+ function likeBook(bookId, likedByUsers) {
+ const patchData = {
+   users: likedByUsers
+ };
+ 
+ fetch(`http://localhost:3000/books/${bookId}`, {
+   method: 'PATCH',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(patchData)
+ })
+   .then(response => response.json())
+   .then(updatedBook => {
+     // Update the book details with the new list of users who liked the book
+     const showPanel = document.querySelector('#show-panel');
+     const likedBy = showPanel.querySelector('ul');
+     likedBy.innerHTML = ''; // Clear previous content
+ 
+     updatedBook.users.forEach(user => {
+       const userLi = document.createElement('li');
+       userLi.textContent = user.username;
+       likedBy.appendChild(userLi);
+     });
+   });
+}})
